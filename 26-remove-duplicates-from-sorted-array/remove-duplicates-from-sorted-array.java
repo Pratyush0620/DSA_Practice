1bd3e51bd4e3a24f2d5916ff310efd39be037fda
Pratyush <137057310+Pratyush0620@users.noreply.github.com>
2025-07-09 18:02:01 +0530

    Time: 3 ms (8.95%) | Memory: 44.7 MB (79.01%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -8,13 +8,24 @@ class Solution {
             if(!map.containsKey(nums[i])){
                 map.put(nums[i],i);
                 list.add(nums[i]);
-                count++;
+                count++;                   //Using HashMap and ArrayList
             }
        }
        for(int i=0;i<count;i++){
         nums[i]=list.get(i);
        }
        return count;
-
+        
+        // HashSet<Integer> set=new HashSet<>();
+        // int n=nums.length;
+        // for(int i=0;i<n;i++){
+        //     set.add(nums[i]);
+        // }
+        // int k=set.size();
+        // int j=0;
+        // for(int x:set){
+        //     nums[j++]=x;
+        // }
+        // return k;
     }
 }
