952b055ca9be9f06f27819dacf40d56c6a7121b4
Pratyush <137057310+Pratyush0620@users.noreply.github.com>
2025-07-09 20:16:04 +0530

    Time: 1 ms (99.89%) | Memory: 46.5 MB (17.57%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,21 +1,30 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int n=nums.length;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<n;i++){
-            if(map.containsKey(nums[i])){
-               map.put(nums[i],map.get(nums[i])+1);
-            }else{
-                map.put(nums[i],1);
-            }
+        // int n=nums.length;
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // for(int i=0;i<n;i++){
+        //     if(map.containsKey(nums[i])){
+        //        map.put(nums[i],map.get(nums[i])+1);
+        //     }else{
+        //         map.put(nums[i],1);
+        //     }
             
-        }
+        // }                                                 //Using HashMap so extra space is here
         
-        for(int key:map.keySet()){
-            if(map.get(key)==1){
-                return key;
-            }
+        // for(int key:map.keySet()){
+        //     if(map.get(key)==1){
+        //         return key;
+        //     }
+        // }
+        // return -1;
+
+
+        //Using XOR bit manipulation technique
+
+        int result=0;
+        for(int num:nums){
+            result ^=num;
         }
-        return -1;
+        return result;
     }
 }
