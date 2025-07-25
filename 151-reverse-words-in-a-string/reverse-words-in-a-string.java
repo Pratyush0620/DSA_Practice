95cefe45196189b91297c3d0abc86c9930fa97c8
Pratyush <137057310+Pratyush0620@users.noreply.github.com>
2025-07-25 16:18:13 +0530

    Time: 6 ms (85.62%) | Memory: 42.8 MB (91.96%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,28 +1,29 @@
 class Solution {
     public String reverseWords(String s) {
+        
         Stack<String> stack=new Stack<>();
+        StringBuilder str=new StringBuilder();
+        s=s.trim();
         int n=s.length();
-        int start=0;
-        StringBuilder result=new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            char ch = s.charAt(i);
-            if (ch == ' ') {
-                if (start < i) {
-                    stack.push(s.substring(start, i));
-                }
-                start = i + 1;
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            if(ch!=' '){
+                str.append(ch);
+            }else if(str.length() > 0){
+                stack.push(str.toString());
+                str.setLength(0);
             }
         }
-        if (start < n) {
-            stack.push(s.substring(start));
+        if(str.length() > 0){
+            stack.push(str.toString());
         }
+        str.setLength(0);
+        while(!stack.isEmpty()){
+            str.append(stack.pop());
+            if(!stack.isEmpty()) str.append(" ");
+            
+        }
+        return str.toString();
 
-        while (!stack.isEmpty()) {
-            result.append(stack.pop());
-            if (!stack.isEmpty()) {
-                result.append(" ");
-            }
-        }
-        return result.toString();
     }
 }
