0f78fd5529166932027803faa47b96b8ded240e7
Pratyush <137057310+Pratyush0620@users.noreply.github.com>
2025-07-30 01:13:20 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (68.57%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -11,14 +11,32 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        HashMap<ListNode,Integer> map=new HashMap<>();
-        ListNode curr=head;
-        while(curr!=null){
-            if(map.containsKey(curr)){
-                return curr;
+        // HashMap<ListNode,Integer> map=new HashMap<>();
+        // ListNode curr=head;
+        // while(curr!=null){
+        //     if(map.containsKey(curr)){
+        //         return curr;                    ///Using HashMap
+        //     }
+        //     map.put(curr,1);
+        //     curr=curr.next;
+        // }
+        // return null;
+
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+
+            if(fast==slow){
+                slow=head;
+
+                while(slow!=fast){
+                    fast=fast.next;
+                    slow=slow.next;
+                }
+                return slow;
             }
-            map.put(curr,1);
-            curr=curr.next;
         }
         return null;
     }
