fe70033f530981cf4c510f5a141e1254eadcaa6f
Pratyush <137057310+Pratyush0620@users.noreply.github.com>
2025-07-29 12:54:42 +0530

    Time: 6 ms (47.62%) | Memory: 64.2 MB (59.67%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -10,19 +10,43 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        Stack<Integer> stack=new Stack<>();
-        ListNode curr=head;
-        while(curr!=null){
-            stack.push(curr.val);
-            curr=curr.next;
+        // Stack<Integer> stack=new Stack<>();
+        // ListNode curr=head;
+        // while(curr!=null){
+        //     stack.push(curr.val);
+        //     curr=curr.next;                  //using Stack
+        // }
+        // curr=head;
+        // while(curr!=null){
+        //     if(curr.val!=stack.pop()){
+        //         return false;
+        //     }
+        //     curr=curr.next;
+        // }
+        // return true;
+        // Step 1: Create a reversed copy of the list
+        ListNode reversed = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode newNode = new ListNode(curr.val); // Create a new node
+            newNode.next = reversed;
+            reversed = newNode;
+            curr = curr.next;
         }
-        curr=head;
-        while(curr!=null){
-            if(curr.val!=stack.pop()){
+
+        // Step 2: Compare original and reversed lists
+        ListNode p1 = head;
+        ListNode p2 = reversed;
+
+        while (p1 != null && p2 != null) {
+            if (p1.val != p2.val) {
                 return false;
             }
-            curr=curr.next;
+            p1 = p1.next;
+            p2 = p2.next;
         }
+
         return true;
     }
 }
