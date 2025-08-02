e5bc864c1744228e99d426a6789073571e60c08f
Pratyush <pratyushsamantra2004@gmail.com>
2025-08-02 13:16:26 +0530

    Deleted the class_objects empty folder

--- class_object/Singleton.java
+++ class_object/Singleton.java
@@ -5,6 +5,6 @@ public class Singleton {
     public static void main(String[] args) {
         Singleton1 ss=Singleton1.getInstance();
         Singleton1 ss1=Singleton1.getInstance();
-
+        
     }
 }
